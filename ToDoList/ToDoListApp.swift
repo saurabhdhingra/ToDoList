aef95c022acf12d83f562c13c38aa066fcc44e0d
saurabhdhingra <75309640+saurabhdhingra@users.noreply.github.com>
2024-09-01 10:25:44 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Saurabh Dhingra on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
